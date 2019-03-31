83ab668d2cecef579436d70ea38a04d859dadc52
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-31 23:25:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int == 2
+    return true
+  end
+
+    (2..int-1).each do |d|
+      if (int%d==0)
+        return false
+      end
+    end
+
+  true
+
+end
+
