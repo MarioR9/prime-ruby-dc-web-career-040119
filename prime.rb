46420115aa81e94f5d314f32c0bf5576eba8f02d
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-31 23:24:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,19 @@
 def prime?(int)
-    if int < 2 
-      return false
-  
-  (2..(int - 1)).each do |n|
-    if int % n == 0
-      return false 
-    end
+  if int == 1
+    return false
+  elsif int < 1
+    return false
+  elsif int == 2
+    return true
   end
-   return true
+
+    (2..int-1).each do |divider|
+      if (int%divider==0)
+        return false
+      end
+    end
+
+  true
+
 end
 
